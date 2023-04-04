504374ebef6bf1906cef8ec2f6711cd03e6bf3aa
Juan Pablo Torres Padilla <juan.torres@nepa.com>
2023-04-04 09:42:52 +0200

    added rest of the spec and all tests green

--- src/Anagram.hs
+++ src/Anagram.hs
@@ -2,10 +2,17 @@ module Anagram(anagramsFor)
 where
 
 import Data.List(sort)
+import Data.Char(toLower)
 
 anagramsFor :: String -> [String] -> [String]
 anagramsFor s ss = filter (isAnagram s) ss
 
 
 isAnagram :: String -> String -> Bool
-isAnagram str1 str2 = sort str1 == sort str2 
+isAnagram str1 str2 = lStr1 /= lStr2 && sort lStr1 == sort lStr2 
+    where
+        lStr1 = lowerString str1
+        lStr2 = lowerString str2
+
+lowerString :: String -> String
+lowerString s = map toLower s
--- test/AnagramSpec.hs
+++ test/AnagramSpec.hs
@@ -18,3 +18,21 @@ spec =
 
         it "detects anagram" $
             anagramsFor "listen" ["enlists", "google", "inlets", "banana"] `shouldMatchList` ["inlets"]
+
+        it "detects three anagrams" $
+            anagramsFor "allergy" ["gallery", "ballerina", "regally", "clergy", "largely", "leading"] `shouldMatchList` ["gallery", "regally", "largely"]
+
+        it "does not detect non-anagrams with identical checksum" $
+            anagramsFor "mass" ["last"] `shouldMatchList` []
+        
+        it "detects anagrams using case-insensitive subjetc" $
+            anagramsFor "Orchestra" ["cashregister", "Carthorse", "radishes"] `shouldMatchList` ["Carthorse"]
+
+        it "does not detect a anagram if the original word is repeated" $
+            anagramsFor "go" ["go Go GO"] `shouldMatchList` []
+
+        it "anagrams must use all letters exactly once" $
+            anagramsFor "tapper" ["patter"] `shouldMatchList` []
+        
+        it "words are not anagrams of themselves (case insensitive)" $
+            anagramsFor "BANANA" ["BANANA", "banana", "Banana"] `shouldMatchList` []
